27a9feafab036b863c545146b03953b8ac8d3231
Sarah Harvey <desertriverjem@gmail.com>
2016-11-01 11:08:11 -0600

    got all the tests converted and working, substituting output checks with assert .contains statements

---
+++ src/primes/Primes.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primes;
+
+import static kiss.API.*;
+/**
+ *
+ * @author deser
+ */
+public class Primes {
+    void testIO(){
+        try(Close out = outOpen("sum.dat"))
+        {
+            println("x+y=35");
+        }
+        
+        try(Close vfy = outVerify("sum.dat")){
+            int x=11;
+            int y= 24;
+            println("x+y="+(x+y));
+        }
+        
+    }
+    
+    void testSum(){
+        try(Close in = inProvide(13, EOL, 22, EOL)){
+            try (Close out = outExpect("x+y=35")){
+                printSum();
+            }
+        }
+    }
+    
+    void printSum(){
+        int x = readInteger();
+        int y = readInteger();
+        println("x+y=" + (x+y));
+    }
+    void testSmallPrimes()
+    {
+        Primes primes = new Primes();
+        assert primes.isPrime(2) == true;
+        assert primes.isPrime(3) == true;
+        assert primes.isPrime(5) == true;
+    }
+    
+    void testSmallComposites()
+    {
+        Primes primes = new Primes();
+        assert primes.isPrime(4) == false;
+        assert primes.isPrime(6) == false;
+    }
+    
+    void testBigPrimes(){
+        Primes primes = new Primes();
+        double t0=time();
+        assert primes.isPrime(1_000_003) == true;
+        double smallTime = time() - t0;
+        
+        double t1 = time();
+        assert primes.isPrime(1_000_000_007) == true;
+        double bigTime = time() - t1;
+        
+        assert (bigTime < 100*smallTime);
+    }
+    
+    boolean isPrime(int x){
+        int y = min(x-1, (int) (sqrt(x)+1.0));
+        for (int factor = 2; factor <=y;++factor){
+            if (x%factor ==0) return false;                
+        }
+        return true;
+    }
+    
+    void testHi(){
+        println("Hi");
+    }
+}
