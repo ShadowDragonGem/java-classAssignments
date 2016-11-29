2119f95dc399e0b07be6ec64d43fce862824edf3
Sarah Harvey <desertriverjem@gmail.com>
2016-11-28 22:25:10 -0700

    additions to the shopper

--- src/Assignment5/Shopper.java
+++ src/Assignment5/Shopper.java
@@ -18,5 +18,34 @@ public class Shopper {
     Items bought;
     boolean finished = false;
     
-  
+    class Action extends Thread{
+        public void run(){
+            while(!finished){
+                pause(random(0,3000)*.001);
+                println(name, " bought ", bought.getList());
+            }
+            
+        }
+        
+        
+    }
+    
+    Action action;
+    void finish(){
+        finished = true;
+     
+        try {
+            action.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Shopper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    Shopper(String _name, Items _bought){
+        name = _name;
+        bought = _bought;
+        action = new Action();
+        action.start();
+    }
 }
